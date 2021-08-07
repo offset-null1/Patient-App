8fdab3460b0cc88aa2e11da4b5f46c49d815c071
offset-null1 <nirvindak23@example.com>
2021-08-07 16:34:22 +0530

    final

--- patient_app/patient_info/views.py
+++ patient_app/patient_info/views.py
@@ -21,18 +21,28 @@ def home(request):
             
             if(option == '0'): 
                 p = Patient(first_name=first_name, last_name=last_name, gender=gender, age=age, disease=disease, doctor_name=doctor_name, fees=fees, started_med_on_date=started_med_on_date)
-                p.save()   
+                p.save()  
+                messages.success(request, messages.INFO,"You successfully registered the Patient record") 
             
             elif(option == '1'):
-                if Patient.objects.filter(first_name=first_name).exists() and Patient.objects.filter(last_name=last_name).exists():
-                    p = Patient(first_name=first_name, last_name=last_name, gender=gender, age=age, disease=disease, doctor_name=doctor_name, fees=fees, started_med_on_date=started_med_on_date)
-                    p.save()  
-                    messages.success(request, "You successfully updated the Patient record")
- 
+                try:
+                   if Patient.objects.filter(first_name=first_name).exists() and Patient.objects.filter(last_name=last_name).exists():
+                       p = Patient(first_name=first_name, last_name=last_name, gender=gender, age=age, disease=disease, doctor_name=doctor_name, fees=fees, started_med_on_date=started_med_on_date)
+                       p.save()  
+                       messages.success(request, "You successfully updated the Patient record")
+                except:
+                    messages.error(request, "Patient record doesn't exist with the credentials provided")
    
             else: 
-                p = Patient.objects.get(first_name=first_name, last_name=last_name, gender=gender, age=age, disease=disease, doctor_name=doctor_name, fees=fees, started_med_on_date=started_med_on_date)
-                p.delete()
+                try:
+                    p = Patient.objects.get(first_name=first_name, last_name=last_name, gender=gender, age=age, disease=disease, doctor_name=doctor_name, fees=fees, started_med_on_date=started_med_on_date)
+                    p.delete()
+                    messages.success(request, "You successfully deleted the Patient record")
+                except:
+                    messages.error(request, "Patient record doesn't exist with the credentials provided")
+
+                    
+    
     
     form = Register()
-    return render(request, 'patient_info/index.html', {'form': form})
+    return render(request,template_name='patient_info/index.html', context={'form': form})
